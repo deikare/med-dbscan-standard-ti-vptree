b9ed92c097962df7e454293f99f3c032ff792eab
Damian <damian1_m@op.pl>
2022-12-26 20:20:20 +0100

    added comments to functions

--- include/math.hpp
+++ include/math.hpp
@@ -7,15 +7,32 @@
 #include "consts.hpp"
 
 namespace math {
+/** @brief Function calculates Mikowski distance
+ * @param point1 first point
+ * @param point2 second point
+ * @param m Minkowski distance parameter
+ */
 double minkowskiDist(const DataPoint &point1, const DataPoint &point2,
                      double m);
 
+/** @brief Function calculates mean of values in STL vector */
 double calculateMean(const std::vector<double> &points);
 
+/** @brief Function calculates variance of values in STL vector */
 double calculateVariance(const std::vector<double> &points);
 
+/** @brief Function draws a random non-repeating integer numbers vector from
+ * given range
+ * @param min minimal number in the range (included in drawing)
+ * @param max maximal number in the range (included in drawing)
+ * @param size number of wanted values (cannot be bigger than length of
+ * [min,max] range)
+ * @returns STL vector of random integers from given range (the values are
+ * non-repeating)
+ */
 std::vector<int> getRandNonRepeatingIntVec(int min, int max, int size);
 
+/** @brief Function calculates median of values in STL vector */
 template <typename T>
 double vectorMedian(std::vector<T> &v) {
   if (v.empty()) {
--- include/vp_tree.hpp
+++ include/vp_tree.hpp
@@ -3,6 +3,7 @@
 
 #include "math.hpp"
 
+/** @brief Class stores data of separate nodes of VP tree */
 class Node {
  public:
   int id;  // global index of the point
@@ -13,6 +14,7 @@ class Node {
   void createRightNode() { rightChild = std::make_shared<Node>(); };
 };
 
+/** @brief Struct is used for convenient data returning */
 struct SplitVector {
   std::vector<DataPoint> leftVector;
   std::vector<DataPoint> rightVector;
@@ -20,25 +22,53 @@ struct SplitVector {
   std::vector<std::size_t> rightIndices;  // stores global right indices
 };
 
+/** @brief Class stores VP tree and allows for searching nearest
+ * neighbours, the class doesn't store data vector itself but just operates on
+ * indices of data entries */
 class VPTree {
   std::shared_ptr<Node> root_;
   /** @brief Method returns local vantage point index */
   int getVantagePointIndex_(const std::vector<DataPoint> &points);
+
+  /** @brief Method calculates median of distances between point of given id and
+   * other points from the dataset */
   double getDistMedian_(const std::vector<DataPoint> &points,
                         std::size_t vantage_point_idx_local);
+
+  /** @brief Method divides the dataset into two seprate subsets pairs (values,
+   * indices) based on relation points Minkowski distance to vantage point and
+   * calculated distance median */
   SplitVector splitPointsVector_(const std::vector<DataPoint> &points,
                                  const std::vector<std::size_t> &global_indices,
                                  std::size_t vantage_point_idx_local, int mu);
+
+  /** @brief Helper recursion method for VP tree creation */
   void createTreeRecursive_(std::shared_ptr<Node> sub_root,
                             const std::vector<DataPoint> &points,
                             const std::vector<std::size_t> &global_indices);
+
+  /** @brief Helper recursion method for finding nearest neighbours */
   std::vector<std::size_t> findNeighboursRecursive_(
       std::shared_ptr<Node> sub_root, const std::vector<DataPoint> &points,
       std::size_t point_id, double epsilon);
 
  public:
+  /** @brief Constructor that creates VP tree for given points */
   VPTree(const std::vector<DataPoint> &points);
+
+  /** @brief Method allows for recreation of VP tree for another set of data
+   * @param points is dataset that will be used for creation of VP tree
+   */
   void createTree(const std::vector<DataPoint> &points);
+
+  /** @brief Method allows for finding all nearest neighbours (k-NN+) in given
+   * distance
+   * @param points values of dataset
+   * @param point_id index of point which neighbours are searched for
+   * tree
+   * @param epsilon is the "radius" of Minkowski distance inside which points
+   * are regarded as neighbours
+   */
   std::vector<std::size_t> findNeighbours(const std::vector<DataPoint> &points,
                                           std::size_t point_id, double epsilon);
 };
