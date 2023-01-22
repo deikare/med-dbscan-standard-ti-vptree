9d4d660ac1dd12c69acca8b94aa1ea0606f27558
deikare <bbgolawski@gmail.com>
2023-01-22 01:15:28 +0100

    Implemented simple dbscan

--- include/dbscan.hpp
+++ include/dbscan.hpp
@@ -4,10 +4,10 @@
 #include <vector>
 #include <functional>
 
-#define NOISE 0
+#define NOISE (-1)
 
 using DataPoint = std::vector<double>;
 
-std::map<DataPoint, unsigned long> dbscan(const std::vector<DataPoint>& points,const std::function<double(DataPoint, DataPoint)>& distanceHandler,double eps,unsigned minPts);
+std::map<DataPoint, long> dbscan(const std::vector<DataPoint>& points,const std::function<double(DataPoint, DataPoint)>& distanceHandler,double eps,unsigned minPts);
 
 std::set<DataPoint> neighbours(const DataPoint & point, const std::vector<DataPoint> & points, const std::function<double (const DataPoint&, const DataPoint&)>& distanceHandler, double eps);
--- src/dbscan.cpp
+++ src/dbscan.cpp
@@ -3,31 +3,36 @@
 //
 #include "dbscan.hpp"
 
-std::map<DataPoint, unsigned long> dbscan(const std::vector<DataPoint> & points, const std::function<double(DataPoint, DataPoint)>& distanceHandler, const double eps, const unsigned int minPts) {
-    std::map<DataPoint, unsigned long> result;
-    unsigned long clusterIndex = NOISE;
+std::map<DataPoint, long> dbscan(const std::vector<DataPoint> & points, const std::function<double(DataPoint, DataPoint)>& distanceHandler, const double eps, const unsigned int minPts) {
+    std::map<DataPoint, long> result;
+    long clusterIndex = NOISE;
 
     for (const auto& point : points) {
         if (result.find(point) == result.end()) {
-            auto neighbourSet = neighbours(point, points, distanceHandler, eps);
-            if (neighbourSet.size() < minPts) {
+            auto seeds = neighbours(point, points, distanceHandler, eps);
+            if (seeds.size() < minPts) {
                 result.emplace(point, NOISE);
                 continue;
             }
 
             clusterIndex++;
             result.emplace(point, clusterIndex);
-            neighbourSet.erase(point);
-            for (const auto& neighbour: neighbourSet) {
-                auto label = result.find(neighbour);
-                if (label == result.end()) {
-                    result.emplace(neighbour, clusterIndex);
-                    auto neighboursOfNeighbour = neighbours(neighbour, points, distanceHandler, eps);
-                    if (neighboursOfNeighbour.size() >= minPts)
-                        neighbourSet.merge(neighboursOfNeighbour);
+            seeds.erase(point);
+            while (!seeds.empty()) {
+                auto seedIterator = seeds.begin();
+                auto seed = *(seedIterator);
+
+                auto entry = result.find(seed);
+                if (entry == result.end()) { //seed is unclassified
+                    result.emplace(seed, clusterIndex);
+                    auto seedNeighbours = neighbours(seed, points, distanceHandler, eps);
+                    if (seedNeighbours.size() >= minPts) //seed is core
+                        seeds.merge(seedNeighbours);
                 }
-                else if (label->second == NOISE)
-                    label->second = clusterIndex;
+                else if (entry->second == NOISE)
+                    entry->second = clusterIndex;
+
+                seeds.erase(seed);
             }
         }
     }
@@ -38,9 +43,9 @@ std::map<DataPoint, unsigned long> dbscan(const std::vector<DataPoint> & points,
 std::set<DataPoint> neighbours(const DataPoint &point, const std::vector<DataPoint> &points,
                                const std::function<double(const DataPoint&, const DataPoint&)>& distanceHandler, double eps) {
     std::set<DataPoint> result;
-    for (const auto& neighbourWannabe : points) {
-        if (distanceHandler(neighbourWannabe, point) <= eps)
-            result.emplace(neighbourWannabe);
+    for (const auto& potentialNeighbour : points) {
+        if (distanceHandler(potentialNeighbour, point) <= eps)
+            result.emplace(potentialNeighbour);
     }
     return result;
 }
