54d4857dfb87da279b8694e02e1aeafb2882988e
deikare <bbgolawski@gmail.com>
2023-01-27 15:04:04 +0100

    Add better run

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,6 @@ bool check_file(std::string file_name) {
 }
 
 int main(int argc, char *argv[]) {
-//    auto [TP, TN, rand] = math::calculateRandIndex({0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1}, {0, -1, 0, -1, -1, 0, -1, -1, 0, 0, -1, -1, -1});
-//    TP++;
-//    TN++;
-//    rand++;
     auto time_analyzer = TimeAnalyzer(TIME_PRESCALER::MICRO);
     auto parser = Parser(argc, argv);
     if (parser.cmdOptionExists("-h")) {
@@ -40,35 +36,110 @@ int main(int argc, char *argv[]) {
     if (parser.cmdOptionExists("-i"))
         ignore_lines = stoi(parser.getCmdOption("-i"));
 
+    std::string algorithmVersion = "std";
+    if (parser.cmdOptionExists("-a"))
+        algorithmVersion = parser.getCmdOption("-a");
+
     auto csv_reader = CSVReader();
     auto [data, classes] = csv_reader.getData(filename, true, ignore_lines);
     time_analyzer.saveDurationNow("reading csv");
 
-//    VPTree vp_tree(data);
-//    time_analyzer.saveDurationNow("building VP tree");
-//
-//    auto vec = vp_tree.findNeighbours(data, 3, 2.);
-//    time_analyzer.saveDurationNow("searching k-NN+ in VP tree");
-//
-  auto distanceHandler = [](const std::vector<double>& point1, const std::vector<double>&point2) {
-      return math::minkowskiDist(point1, point2, 2);
-  };
 
     double eps = 1; //try 1, 3
+    if (parser.cmdOptionExists("-e"))
+        eps = stod(parser.getCmdOption("-e"));
+
     unsigned int minPts = 2;
-  std::vector<double> refPoint = {0, 0};
-  std::vector<ReferencePointType> references = {MIN, MAX};
+    if (parser.cmdOptionExists("-m"))
+        minPts = stoi(parser.getCmdOption("-m"));
 
+    std::string baseFilename = filename.substr(filename.find_last_of("/\\") + 1);
+    std::string::size_type const p(baseFilename.find_last_of('.'));
+    std::string baseFilenameNoExtension = baseFilename.substr(0, p);
 
-    DBScan result = DBScan(data, distanceHandler, minPts, eps);
-//  DBScanTi result = DBScanTi(data, distanceHandler, eps, minPts, refPoint);
-//  DBScanTi result = DBScanTi(data, distanceHandler, eps, minPts, MAX);
-//  DBScanTi result = DBScanTi(data, distanceHandler, eps, minPts, MIN);
-//  DBScanTi result = DBScanTi(data, distanceHandler, eps, minPts, references);
-//    DBScanVPTree result = DBScanVPTree(data, minPts, eps);
-    result.printResultToFile("../data/dbscan-result-new.csv");
-    result.generateOutFile("../data", "data");
-    result.generateStatFile("../data", "data", classes);
+    if (algorithmVersion == "std") {
+        auto distanceHandler = [](const std::vector<double>& point1, const std::vector<double>&point2) {
+            return math::minkowskiDist(point1, point2, 2);
+        };
+
+        DBScan result = DBScan(data, distanceHandler, minPts, eps);
+        result.printResultToFile("../data/dbscan-result-new.csv");
+        result.generateOutFile("../data", baseFilenameNoExtension);
+        result.generateStatFile("../data", baseFilenameNoExtension, classes);
+    }
+
+    else if (algorithmVersion == "ti") {
+        auto distanceHandler = [](const std::vector<double>& point1, const std::vector<double>&point2) {
+            return math::minkowskiDist(point1, point2, 2);
+        };
+
+        std::string refPointVersion = "max";
+        if (parser.cmdOptionExists("-rv"))
+            refPointVersion = parser.getCmdOption("-rv");
+
+        DBScanTi* result;
+
+        if (refPointVersion == "max")
+            result = new DBScanTi(data, distanceHandler, eps, minPts, MAX);
+
+        else if (refPointVersion == "min")
+            result = new DBScanTi(data, distanceHandler, eps, minPts, MIN);
+
+        else {
+            std::vector<std::string> refArgs;
+            std::stringstream stream(refPointVersion);
+            std::string segment;
+            while (std::getline(stream, segment, '=' ))
+                refArgs.emplace_back(segment);
+
+            if (refArgs.size() != 2) {
+                std::cout << "Wrong refArg format";
+                exit(-4);
+            }
+
+            if (refArgs[0] == "point") {
+                std::vector<double> refPoint;
+                std::stringstream attributesStream(refArgs[1]);
+                while (std::getline(attributesStream, segment, ','))
+                    refPoint.emplace_back(stod(segment));
+
+                result = new DBScanTi(data, distanceHandler, eps, minPts, refPoint);
+            }
+
+            else if (refArgs[0] == "refOpts") {
+                std::vector<ReferencePointType> references;
+                std::stringstream referenceStream(refArgs[1]);
+                while (std::getline(referenceStream, segment, ',')) {
+                    if (segment == "min")
+                        references.emplace_back(MIN);
+                    else if (segment == "max")
+                        references.emplace_back(MAX);
+                    else {
+                        std::cout << "Wrong references option format";
+                        exit(-4);
+                    }
+                }
+
+                result = new DBScanTi(data, distanceHandler, eps, minPts, references);
+            }
+            else {
+                std::cout << "Wrong ref option";
+                exit(-4);
+            }
+        }
+
+        result->printResultToFile("../data/dbscan-result-new.csv");
+        result->generateOutFile("../data", baseFilenameNoExtension);
+        result->generateStatFile("../data", baseFilenameNoExtension, classes);
+    }
+
+    else if (algorithmVersion == "vp") {
+        DBScanVPTree result = DBScanVPTree(data, minPts, eps);
+
+        result.printResultToFile("../data/dbscan-result-new.csv");
+        result.generateOutFile("../data", baseFilenameNoExtension);
+        result.generateStatFile("../data", baseFilenameNoExtension, classes);
+    }
 
     time_analyzer.printRaport();
 
